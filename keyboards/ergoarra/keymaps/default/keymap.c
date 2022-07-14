c3dbc5c32225a4ed669e1d84dce8301b549262cc
romulogarofalo <romulogarofalo@gmail.com>
2022-07-13 23:44:46 -0300

    finished

--- keyboards/ergoarra/keymaps/default/keymap.c
+++ keyboards/ergoarra/keymaps/default/keymap.c
@@ -12,19 +12,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERT] = LAYOUT(
 		// left rand
-		KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,     KC_MINS,
+		KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,
 		KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_PIPE,
 		KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,           
 		KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,
-		KC_LCTL,  KC_APP, KC_LALT, SYMB_L,  OTHER_L,                  
-			      KC_SPC, KC_BSPC, KC_ENT,  KC_DEL,  KC_HOME,  KC_END,
+		KC_LCTL,  KC_APP, KC_LALT, SYMB_L,  OTHER_L,                
+			      							                  KC_SPC,  KC_PGUP, 
+																       KC_PGDN,
+				  								     KC_SPC,  KC_BSPC, KC_DEL,
 		// rigth rand
 		OTHER_L,   KC_6,   KC_7,      KC_8,    KC_9,   KC_0,     KC_BACKSPACE, 
-		KC_GRV,    KC_Y,   KC_U,      KC_I,    KC_O,   KC_P,     KC_LBRC, 
+		KC_EQL,    KC_Y,   KC_U,      KC_I,    KC_O,   KC_P,     KC_BSLS, 
 		           KC_H,   KC_J,      KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
 		KC_RBRC,   KC_N,   KC_M,      KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
 		                   KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT, KC_RGHT,
-		KC_HOME,   KC_END, KC_LSFT,   KC_DEL, KC_ENT,  KC_BSPC
+		KC_HOME, KC_END, 
+		KC_LSFT,   
+		KC_DEL,  KC_ENT, KC_SPC
 	),
 
 	[_SYMB] = LAYOUT(
@@ -50,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,    KC_T,    KC_SPC,
 		KC_LSFT, KC_A,    KC_S,   KC_D,   KC_F,    KC_G,           
 		KC_LCTL, KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,    KC_SPC,
-		KC_SPC,  KC_SPC,  KC_SPC, KC_SPC, KC_SPC,                  
+		KC_SPC,  KC_SPC,  KC_PSCR, KC_SPC, KC_SPC,                  
 			     KC_VOLU,  KC_VOLD, KC_SPC, KC_MUTE,  KC_WAKE, KC_SLEP,
 		// rigth rand
 		KC_SPC,  KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_BACKSPACE, 
